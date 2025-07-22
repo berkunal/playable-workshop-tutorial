451a3919c7d2ba71d0aca62efd88df6bc5a5d61b
Berk Ünal <cesmeli.berk@gmail.com>
2025-07-22 22:52:18 +0300

    Initial commit

---
+++ scripts/camera_rig.gd
@@ -0,0 +1,31 @@
+extends SpringArm3D
+
+
+@onready var camera := $Camera3D
+var turn_rate := 150
+var mouse_sensitivity := 0.1
+var mouse_input := Vector2()
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	spring_length = camera.position.z
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var look_input := Input.get_vector("view_right", "view_left", "view_down", "view_up")
+	look_input = look_input * turn_rate * delta
+	look_input += mouse_input
+	mouse_input = Vector2()
+
+	rotation_degrees.x += look_input.y
+	rotation_degrees.y += look_input.x
+	rotation_degrees.x = clampf(rotation_degrees.x, -70, 50)
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		mouse_input = -event.relative * mouse_sensitivity
+	elif event is InputEventKey and event.keycode == KEY_ESCAPE and event.pressed:
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	elif event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
